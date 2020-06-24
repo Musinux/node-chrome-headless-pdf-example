66c7655b39f2f24282f5a52658f39537590a9de6
Louis Cherel <cherel.louis@gmail.com>
2020-06-24 09:27:16 +0200

    remove useless stuff

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,6 @@
 var express = require('express');
-const { route } = require('./users');
 var router = express.Router();
 const renderPDF = require('chrome-headless-render-pdf')
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 router.get('/api/pdf/generate', (req, res, next) => {
 
